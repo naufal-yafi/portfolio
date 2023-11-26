9217951a33aa7d079b88b2b68cfe070f7b5e7dbd
naufal-yafi <muhammadnaufalyafi00@gmail.com>
2023-11-26 20:08:15 +0700

    prject display

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,5 +1,6 @@
 import { limitText } from "@/libs/util";
 import iconArrowRightUpBlack from "@image/arrow-right-up-black.svg";
+import Image from "next/image";
 import Button from "./Button";
 
 const ProjectCard = (props: any) => {
@@ -13,19 +14,30 @@ const ProjectCard = (props: any) => {
         className={`w-[300px] h-[500px] ${
           props.disable
             ? "bg-gradient-to-b sm:bg-gradient-to-r from-neutral-800 to-neutral-900"
-            : "bg-neutral-800 group-hover:w-[500px] group-hover:h-[280px] sm:group-hover:h-[320px] transition-all duration-500"
+            : "bg-neutral-800 group-hover:w-[500px] group-hover:h-[280px] sm:group-hover:h-[320px] transition-all duration-500 cursor-zoom-in"
         }`}
-      ></div>
+      >
+        <div className="flex w-full h-full justify-center items-center">
+          <Image
+            src={props.img}
+            alt="project display"
+            width={1920}
+            height={960}
+            className="object-cover h-full w-full flex justify-center items-center"
+            quality={100}
+            loading="lazy"
+          />
+        </div>
+      </div>
       {props.disable ? null : (
         <div className="mt-[75px]">
-          <p className="mb-5">
-            {limitText("I made this project to learn nextjs and tailwind.", 60)}
-          </p>
+          <p className="mb-5">{limitText(props.title, 60)}</p>
           <Button
             color="yellow"
             icon={iconArrowRightUpBlack}
             openNewTab={false}
             right_position_icon={true}
+            link={props.link}
           >
             See Project
           </Button>
@@ -36,7 +48,10 @@ const ProjectCard = (props: any) => {
 };
 
 ProjectCard.defaultProps = {
+  title: "Your Title",
   disable: false,
+  link: "",
+  img: "",
 };
 
 export default ProjectCard;
